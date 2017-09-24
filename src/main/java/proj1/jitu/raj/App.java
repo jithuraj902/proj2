18ee575a5f3263d5f1d8843bb3d5d46534a13ff5
jithu <jithuraj902@gmail.com>
2017-09-24 15:53:38 +0000

    my second proj

---
+++ src/main/java/proj1/jitu/raj/App.java
@@ -0,0 +1,13 @@
+package proj1.jitu.raj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello this is the second example" );
+    }
+}
